f3d6643070a390c09c11aeeea769325f09b82c84
Kagero1 <charles_stepven_lin@dlsu.edu.ph>
2020-01-12 19:00:01 +0800

    Newer Version

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -17,13 +17,18 @@ $(document).ready(() => {
 
     var datearray = today[0].split("/");
     var newdate = datearray[2] + '-' + datearray[1] + '-' + datearray[0];
-    var time = today[1].substring(0, today[1].length-1).substring(1);
+    var newDate = datearray[1] + '/' + datearray[0] + '/' + datearray[2] + " ";
+    var time = today[1].substring(1);
+    console.log(time);
     var newTime;
     if(time.includes("am")){
         var temp = time.split(":");
-        newTime = "0" + temp[0] + temp[1] + temp[2].substring(0, temp[2].length-2);
+        newTime = "0" + temp[0] + ":" +  temp[1];
+        console.log(newTime);
     }else if(time.includes("pm")){
-        newTime = time.substring(0, time.length-2);
+        var temp = time.split(":");
+        newTime = (parseInt(temp[0]) + 12).toString() + ":" + temp[1];
+        console.log(newTime);
     }
 
     var ttoday = newdate + "T" + newTime;
@@ -35,14 +40,20 @@ $(document).ready(() => {
         var id = $(this).attr("data-id");
         var date = new Date( $("#dateField").val());
         console.log(date);
+
+        console.log(new Date(newDate + newTime));
+        
         if($("#qtyField").val() > 0 
             && date.getDate() > 0 
             && date.getMonth() + 1 > 0 
-            && date.getFullYear() > 0){
+            && date.getFullYear() > 0
+            && date > new Date(newDate + newTime)){
             console.log("Quantity: " + $("#qtyField").val() 
             + "\nYear: " + date.getFullYear()
             + "\nMonth: " + date.getMonth() + 1
             + "\nDay: " + date.getDate());
+
+            $(".editError").remove();
             
             if($("#qtyField").val() > 0 && $("#qtyField").val() < $("#currentField").val()){
                 var item={}
@@ -67,10 +78,10 @@ $(document).ready(() => {
             
         }else{
             if($(".editError").length == 0){
-                $(".errorMessage").append("<label class='editError' style='color: red;'>Please fill up empty fields.</label>");
+                $(".errorMessage").append("<label class='editError' style='color: red;'>Please input valid values.</label>");
             }else{
                 $(".editError").remove();
-                $(".errorMessage").append("<label class='editError' style='color: red;'>Fields cannot be empty.</label>");
+                $(".errorMessage").append("<label class='editError' style='color: red;'>Please double check your inputs.</label>");
             }
         }
         
